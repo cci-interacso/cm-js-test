10bec5b277f884b9a021945727077b6823df2462
emiliodelatorre <58807883+emiliodelatorre@users.noreply.github.com>
2020-02-04 12:15:01 +0100

    cmv-119: Permit players to be used by External user (#17)
    * cmv-119: Permit players to be used by External user
    
    * cmv-119: Permit players to be used by External user

---
+++ features/step_definitions/PermitPlayersSteps.ts
@@ -0,0 +1,11 @@
+import { Then } from "cucumber";
+import { actorCalled } from "@serenity-js/core";
+import { AssignInventory } from "../../src/screenplay/ui/tasks/AssignInventory";
+
+Then(/(.*) can click on Screen and assign it to the Seville group/, function(
+  actorName: string
+) {
+  return actorCalled(actorName).attemptsTo(
+    AssignInventory.addInventory("BALHAM STN DEPOT")
+  );
+});
--- src/screenplay/ui/tasks/AssignInventory.ts
+++ src/screenplay/ui/tasks/AssignInventory.ts
@@ -1,29 +1,34 @@
-import { Task } from "@serenity-js/core";
-import { Inventory } from "../po/inventory";
-import { Click, Enter, Hover } from "@serenity-js/protractor";
-import { Library } from './../po/library'
-
+import { Task, Duration } from "@serenity-js/core";
+import { Inventory } from "../po/Inventory";
+import {
+  Click,
+  Enter,
+  Hover,
+  Wait,
+  isClickable
+} from "@serenity-js/protractor";
 
 export class AssignInventory implements Task {
+  constructor(private inventory: string) {}
 
-    constructor(private inventory: string) {
+  static addInventory(inventory: string): AssignInventory {
+    return new AssignInventory(inventory);
+  }
 
-    }
-
-    static addInventory(inventory: string): AssignInventory {
-        return new AssignInventory(inventory)
-    }
-
-    performAs(actor: import("@serenity-js/core").PerformsActivities): PromiseLike<void> {
-        return actor.attemptsTo(
-            Click.on(Inventory.INVENTORY),
-            Enter.theValue(this.inventory).into(Library.SEARCH_BY_NAME),
-            Hover.over(Inventory.HOVER_ON_PLAYER),
-            Click.on(Inventory.ASSIGN_TO_SEVILLE),
-            Click.on(Inventory.ASSIGN_CAMPAIGN)
-
-        )}
-
-
-
+  performAs(
+    actor: import("@serenity-js/core").PerformsActivities
+  ): PromiseLike<void> {
+    return actor.attemptsTo(
+      Click.on(Inventory.INVENTORY),
+      Enter.theValue(this.inventory).into(Inventory.SEARCH_BY_NAME),
+      Hover.over(Inventory.HOVER_ON_PLAYER),
+      Wait.upTo(Duration.ofSeconds(3)).until(
+        Inventory.ASSIGN_USERGROUP,
+        isClickable()
+      ),
+      Click.on(Inventory.ASSIGN_USERGROUP),
+      Click.on(Inventory.ASSIGN_TO_SEVILLE),
+      Click.on(Inventory.ASSIGN)
+    );
+  }
 }
