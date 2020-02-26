0e917bbdd1f3985524fd8696553fcec3e3e03194
Niyi Falade <niyijobs@gmail.com>
2020-02-26 10:59:51 +0000

    cmv-fix

--- src/screenplay/api/endpoints/requests/CampaignRequest.ts
+++ src/screenplay/api/endpoints/requests/CampaignRequest.ts
@@ -17,20 +17,23 @@ export var campaignRequest = {
 export const campaignRequestAlreadyStarted = {
     name: faker.company.companyName(),
     fromDate: date.format(past, 'YYYY-MM-DD'),
-    toDate: date.format(next_month, 'YYYY-MM-DD')
+    toDate: date.format(next_month, 'YYYY-MM-DD'),
+    campaignId: faker.random.number()+faker.random.alphaNumeric()
 }
 
 export const campaignRequestFutureDate = {
     name: faker.company.companyName(),
     fromDate: date.format(future, 'YYYY-MM-DD'),
-    toDate: date.format(next_month, 'YYYY-MM-DD')
+    toDate: date.format(next_month, 'YYYY-MM-DD'),
+    campaignId: faker.random.number()+faker.random.alphaNumeric()
 }
 
 
 export const campaignRequestEndDateInthePast = {
     name: faker.company.companyName(),
     fromDate: date.format(past, 'YYYY-MM-DD'),
-    toDate: date.format(past_1, 'YYYY-MM-DD')
+    toDate: date.format(past_1, 'YYYY-MM-DD'),
+    campaignId: faker.random.number()+faker.random.alphaNumeric()
 }
 
 
