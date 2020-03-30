c85c3ae05483adce144ee4e7a31106a5d02c705d
Emilio de la Torre <emilio.delatorre@interacso.com>
2020-03-30 13:51:56 +0200

    Increase GET screens limit

--- src/screenplay/cm_variables.ts
+++ src/screenplay/cm_variables.ts
@@ -1,28 +1,25 @@
 export const Status = {
-    DRAFT: 1,
-    PAUSED: 2,
-    ONGOING: 3
-}
+  DRAFT: 1,
+  PAUSED: 2,
+  ONGOING: 3
+};
 
 export const campaignPath = {
-    CAMPAIGNS: '/campaigns',
-    GET_CAMPAIGNS: '/campaigns/',
-    GET_CREATIVES: '/creatives/static?userGroupsDetail=false'
-}
-
+  CAMPAIGNS: "/campaigns",
+  GET_CAMPAIGNS: "/campaigns/",
+  GET_CREATIVES: "/creatives/static?userGroupsDetail=false"
+};
 
 export const enum Path {
-    campaigns = '/campaigns',
-    getCampaigns = '/campaigns',
-    getCreatives = '/creatives/static?userGroupsDetail=false',
-    getGroups = '/okta/groups',
-    addStaticContent = '/creatives/static',
-    addstaticContentToDefaultSchedule = '/campaigns/',
-    schedules ='/schedules/',
-    screens = '/screens?userGroupsDetail=false&limit=4',
-    templates ='/templates/',
-    template = '/template/',
-    screen = '/screens/'
+  campaigns = "/campaigns",
+  getCampaigns = "/campaigns",
+  getCreatives = "/creatives/static?userGroupsDetail=false",
+  getGroups = "/okta/groups",
+  addStaticContent = "/creatives/static",
+  addstaticContentToDefaultSchedule = "/campaigns/",
+  schedules = "/schedules/",
+  screens = "/screens?userGroupsDetail=false&limit=10000",
+  templates = "/templates/",
+  template = "/template/",
+  screen = "/screens/"
 }
-
-
